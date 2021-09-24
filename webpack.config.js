c5dde342edce1a0f2702425a8119200dfbf206b3
Tom Streller <tom@streller.me>
2021-09-24 11:55:15 +0200

    Improved build output

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,15 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { SubresourceIntegrityPlugin } = require('webpack-subresource-integrity');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 const { ESBuildMinifyPlugin } = require('esbuild-loader');
+
 const path = require('path');
 
+const srcDir = path.join(__dirname, 'src');
+const outDir = path.join(__dirname, 'dist');
+
 module.exports = {
-  entry: './src/index.tsx',
+  entry: path.join(srcDir, 'index.tsx'),
   devtool: 'inline-source-map',
   module: {
     rules: [
@@ -19,20 +25,34 @@ module.exports = {
     ],
   },
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[contenthash].js',
+    path: outDir,
+    crossOriginLoading: 'anonymous',
+    clean: true
   },
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html',
+      template: path.join(srcDir, 'index.html'),
       title: 'text-engine',
       inject: 'body',
     }),
+    new SubresourceIntegrityPlugin(),
+    new FaviconsWebpackPlugin(path.join(srcDir, 'logo.svg')),
   ],
   optimization: {
+    runtimeChunk: 'single',
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all',
+        },
+      },
+    },
     minimizer: [
       new ESBuildMinifyPlugin({
         target: 'es2015', // Syntax to compile to (see options below for possible values)
